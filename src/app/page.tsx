b68ecbd236bbaeb90ec5ed6630ae4efadcf4dfb9
nishanth <mksabarinishanth@gmail.com>
2025-09-16 00:27:39 +0530

    Added Heading and subheading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,6 @@
 "use client";
 
 import { ProductCardDisplayPage } from "@/ProductCardDispalyPage/ProductCardDisplayPage";
-import FirstPart from "@/productDescriptionPageComponent/firstPart/FirstPart";
-import ProductCarousel from "@/productDescriptionPageComponent/firstPart/ImageProductCarosel.";
-import TextDescription1part from "@/productDescriptionPageComponent/firstPart/TextDescription1part";
-import ProductDescriptionPageComponent from "@/productDescriptionPageComponent/productDescriptionPageComponent";
-import { Link } from "lucide-react";
 
 export default function Home() {
   return (
--- src/productDescriptionPageComponent/productDescriptionPageComponent.tsx
+++ src/productDescriptionPageComponent/productDescriptionPageComponent.tsx
@@ -14,11 +14,16 @@ export default function ProductDescriptionPageComponent({ id }) {
   const ID = Number(id);
   return (
     <div className={styles.container}>
+      <h1 className={styles.firstHeading}>Kepler-442a</h1>
       <FirstPart id={id}></FirstPart>
       <SecondPart id={ID}></SecondPart>
+      <h3 className={styles.subHeading}>Explore every detail</h3>
       <ThirdPart id={ID} />
+      <h3 className={styles.subHeading}>People also bought this</h3>
       <FourthPart id={ID}></FourthPart>
+      <h3 className={styles.subHeading}>About this item</h3>
       <SixthPart id={ID}></SixthPart>
+      <h3 className={styles.subHeading}>From the manufacturer</h3>
       <FifthPart id={ID}></FifthPart>
     </div>
   );
